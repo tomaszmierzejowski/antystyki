6e65a0a1b454c70ded9a6cf2804a245c3e712310
Tomasz Mierzejowski <tmierzejowski@gmail.com>
2025-10-26 14:57:17 +0100

    init pipeline

--- backend/Antystics.Api/Program.cs
+++ backend/Antystics.Api/Program.cs
@@ -43,7 +43,6 @@ builder.Services.AddSwaggerGen(c =>
     });
 });
 
-
 // Configure Database
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") 
     ?? "Host=localhost;Database=antystics;Username=postgres;Password=postgres";
